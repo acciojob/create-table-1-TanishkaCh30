a1e4622f96056ee729996bd87d27d7179fdad211
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-01-29 20:15:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let sample = document.querySelector("#sampleTable");
 function insert_Row() {
-    //Write your code here
-  
+    let newRow = sample.insertRow(0);
+	let newCell1 = newRow.insertCell(0);
+	let newCell2 = newRow.insertCell(1);
+
+	newCell1.textContent="New Cell1";
+	newCell2.textContent="New Cell2";
+	
   
 }
